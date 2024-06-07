bb8e2f3b801b3b20149bc7d9550a3713c17dc97a
YeisonDorado <yeisondorado96@gmail.com>
2024-06-07 18:07:32 -0500

    Se crea la estructura con la coleccion categorias del microservicio contabilidad

---
+++ main.py
@@ -0,0 +1,94 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+import json
+from waitress import serve
+
+from Controladores.ControladorCategoria import ControladorCategoria
+
+app=Flask(__name__)
+"""
+los cors permiten hacer pruebas al servidor desde la misma maquina en la que esta corriendo el proyecto 
+"""
+cors = CORS(app)
+
+
+
+"""
+Implementacion de los controladores--------------------------------
+"""
+
+
+miControladorCategoria=ControladorCategoria()
+
+
+
+"""
+----------------------------------------------SERVICIOS CATEGORIAS___________________________________________________________________________
+"""
+
+@app.route("/categorias",methods=['GET'])
+def getCategorias():
+    json=miControladorCategoria.index()
+    return jsonify(json)
+
+@app.route("/categorias",methods=['POST'])
+def crearCategoria():
+    data = request.get_json()
+    json=miControladorCategoria.create(data)
+    return jsonify(json)
+
+@app.route("/categorias/<string:id>",methods=['GET'])
+def getCategoria(id):
+    json=miControladorCategoria.show(id)
+    return jsonify(json)
+
+@app.route("/categorias/<string:id>",methods=['PUT'])
+def modificarCategoria(id):
+    data = request.get_json()
+    json=miControladorCategoria.update(id,data)
+    return jsonify(json)
+
+@app.route("/categorias/<string:id>",methods=['DELETE'])
+def eliminarCategoria(id):
+    json=miControladorCategoria.delete(id)
+    return jsonify(json)
+
+
+
+
+
+
+
+
+"-----------------------------------------------------------------------------"
+"""
+Servicio que el servidor ofrecerá, y este consiste en retornar un JSON el cual
+tiene un mensaje que dice que el servidor está corriendo.
+"""
+
+@app.route("/",methods=['GET'])
+def test():
+    json = {}
+    json["message"]="Server running ..."
+    return jsonify(json)
+
+"""
+Método leer el archivo de configuración del proyecto,
+retornará un diccionario el cual posee la información dentro del
+JSON y se podrá acceder a los atributos necesarios.
+"""
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
+
+if __name__ == '__main__':
+    dataConfig = loadFileConfig()
+    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
+    """
+       Se crea la instancia del servidor con la url del backend y puerto especificado 
+       en el archivo de configuración.
+       """
+    serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
